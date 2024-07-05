e8f679e360d1c5eb7d1ae8a3835ac33ae33b44c5
Amos Oluoch <aoluoch271@gmail.com>
2024-07-05 10:47:38 +0300

    em

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Amos OLuoch',
+    streetAddress: '123 Main St'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      employee,
+      [key]: value
+    };
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+
+  
+  
